8e3d6f9547a0bcfe2cfc3df3e8b827ad39a6939b
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-06-30 12:37:39 +0100

    create api.py

--- application/api.py
+++ application/api.py
@@ -2,11 +2,9 @@ from flask import Blueprint
 from flask import Flask, render_template, url_for, redirect, request, session, jsonify, flash, Blueprint
 from .database import DataBase
 
-view = Blueprint("views", __name__)
-
+api = Blueprint("api", __name__)
 
 # GLOBAL CONSTANTS
+
 NAME_KEY = 'name'
 MSG_LIMIT = 20
-
-# VIEWS
